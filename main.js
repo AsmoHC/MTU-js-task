ad6e55dac73a11f2e2844faa3bc66ca2d07a65d5
AsmoHC <parasmod7852@gmail.com>
2020-11-17 05:05:38 +0200

    blocks in game-field

--- main.js
+++ main.js
@@ -29,3 +29,39 @@ function countdown(element) {
         seconds--;
     }, 1000);
 }
+
+
+//blocks creating
+
+(function() {
+    "use strict";
+
+    function createDiv() {
+        var blockDiv = document.createElement("div");
+        blockDiv.className = "new-div";
+        return blockDiv;
+    }
+
+    function createAndModifyDivs() {
+        var test = document.getElementById("game-field"),
+            myDivs = [],
+            i = 0,
+            numOfDivs = 35;
+
+        for (i; i < numOfDivs; i += 1) {
+            myDivs.push(createDiv());
+            test.appendChild(myDivs[i]);
+        }
+    }
+    createAndModifyDivs();
+
+    document.querySelectorAll('#game-field>div').forEach(el => {
+        el.style.backgroundColor = '#' + Math.random().toString(16).slice(2, 8);
+    })
+}());
+
+// animation blocks appear
+
+$('.new-div:eq(0)').fadeIn(300, function() {
+    $(this).next().fadeIn(300, arguments.callee);
+});
