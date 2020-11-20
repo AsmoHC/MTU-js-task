f57f405f3e8ec0a7ac2bb09b30e63a287851cd04
AsmoHC <parasmod7852@gmail.com>
2020-11-20 17:55:35 +0200

    major changing

--- main.js
+++ main.js
@@ -1,56 +1,105 @@
-"use strict";
-// timer countdown
-var cdtime;
-var minutes = 1;
-var seconds = 00;
+var startButton = document.getElementById('start-button'),
+    gameField = document.getElementById('game-field'),
+    scoreElement = document.getElementById('score'),
+    score = 0,
+    leaderBoardElement = document.getElementById('leaderboard-list');
+timer;
 
-function countdown(element) {
-    cdtime = setInterval(function() {
-        var timer = document.getElementById(element);
-        if (seconds == 0) {
-            if (minutes == 0) {
-                alert(timer.innerHTML = "game over");
-                clearInterval(cdtime);
-                return;
-            } else {
-                minutes--;
-                seconds = 60;
-            }
-        }
-        var secondstxt;
-        if (seconds > 1) {
-            secondstxt = 'seconds';
-        } else {
-            secondstxt = 'second';
-        }
-        timer.innerHTML = seconds + ' ' + secondstxt;
-        seconds--;
-    }, 1000);
-};
+function randomPosX(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-// animation random blocks appear
-$.fn.shuffle = function() {
-    var m = this.length,
-        t, i;
-    while (m) {
-        i = Math.floor(Math.random() * m--);
-        t = this[m];
-        this[m] = this[i];
-        this[i] = t;
+function randomPosY(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomAmountOfCubes() {
+    return Math.floor(Math.random() * 3);
+}
+
+function createCube() {
+    var cube = document.createElement('div'),
+        x = randomPosX(600),
+        y = randomPosY(400);
+    cube.className = 'cube';
+    cube.style.left = x + 'px';
+    cube.style.top = y + 'px';
+    cube.style.backgroundColor = '#' + Math.random().toString(16).slice(2, 8);
+    cube.addEventListener('click', function cubeClicked() {
+        score++;
+        scoreElement.innerHTML = 'Score: ' + score;
+        gameField.removeChild(cube);
+        addCubes();
+    });
+    return cube;
+}
+
+function addCubes() {
+    var amountOfCubes = getRandomAmountOfCubes(),
+        cubes = [];
+
+    for (var i = 0; i < amountOfCubes; i += 1) {
+        var newCube = createCube();
+        cubes.push(newCube);
+        gameField.appendChild(newCube);
+
     }
-    return this;
-};
-$('.new-div').shuffle().each(function(n) {
-    $(this).delay(n * 900).fadeTo(1500, 1);
-});
+
+    cubes.forEach(animateCube);
+
+    // In case we are out of cubes on the game field
+    if (gameField.children.length === 0) {
+        var additionalCube = createCube();
+        gameField.appendChild(additionalCube);
+        animateCube(additionalCube);
+    }
+
+}
+
+function animateCube(cube) {
+    setTimeout(function() {
+        cube.style.opacity = 1;
+    }, 10);
+}
+
+function setTimer() {
+    var seconds = 7,
+        timerElement = document.getElementById('timer');
+
+    timer = setInterval(function() {
+        --seconds;
+        timerElement.innerHTML = seconds;
+        if (seconds === 0) {
+            timerElement.innerHTML = 0;
+            // to see 0 in the timer before the alert shows
+            setTimeout(function() {
+                clearInterval(timer);
+                alert('Game Over!');
+                addRecordToALeaderboard(score);
+                score = 0;
+                scoreElement.innerHTML = 'Score: 0';
+                startButton.disabled = false;
+                timerElement.innerHTML = 60;
+                clearGameField();
+            }, 5);
+        }
+    }, 1000)
+
+}
+
+function addRecordToALeaderboard(score) {
+    var scoreItem = document.createElement('li');
+    scoreItem.innerHTML = score;
+    leaderBoardElement.appendChild(scoreItem);
+}
+
+function clearGameField() {
+    gameField.innerHTML = '';
+}
 
 
-// let heart = document.querySelector('.heart');
-// let likesNumber = document.querySelector('.likes-number');
-// let counter = 0;
-
-// heart.onclick = function() {
-//     counter++;
-//     likesNumber.textContent = counter;
-//     heart.classList.toggle('added');
-// };
+function startGame() {
+    startButton.disabled = true;
+    addCubes();
+    setTimer();
+}
